1a988f0de453a08fc36efca2790a61bb3cdd14c1
Andrei Mikhailouski <andrei.mikhailouski@devteam.inc>
2024-03-03 20:11:52 +0300

    add req

--- BackendTestTask.Services/Models/Node/RequestRemoveNodeModel.cs
+++ BackendTestTask.Services/Models/Node/RequestRemoveNodeModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace BackendTestTask.Services.Models.Node
 {
     public class RequestRemoveNodeModel
     {
+        [Required]
         public string TreeName { get; set; }
+        [Required]
         public int NodeId { get; set; }
     }
 }
